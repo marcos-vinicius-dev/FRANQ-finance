0908c301d5a3756ab37ac26313d5d2bdb4daf9a8
Marcos Vinicius <marcos.silva@athonenergia.com.br>
2025-03-16 23:35:58 -0300

    feat: adiciona provider BrowserRouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import { BrowserRouter } from "react-router";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const rootElement = document.getElementById("root");
+
+if (rootElement) {
+  createRoot(rootElement).render(
+    <StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </StrictMode>,
+  );
+} else {
+  console.error("Failed to find the root element");
+}
