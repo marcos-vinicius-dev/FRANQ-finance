4a81625e6642d49b06f12cbdbb344be019021df3
Marcos Vinicius <marcos.silva@athonenergia.com.br>
2025-03-18 00:52:32 -0300

    feat(routes): adiciona configuração de rotas para a aplicação
    - Adicionada rota inicial (/) para a página de login.
    - Adicionada rota /cadastro para a página de criação de conta.
    - Adicionada rota /visao-geral para o dashboard.
    - Adicionada rota /cotacoes para a lista de cotações.

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
-import { BrowserRouter } from "react-router";
+import { BrowserRouter, Route, Routes } from "react-router";
 import "./styles/index.css";
+import LoginPage from "./pages/auth/login.tsx";
+import CreateAccountPage from "./pages/auth/create-account.tsx";
+import { DashboardPage } from "./pages/quotes/dashboard.tsx";
+import { QuoteListPage } from "./pages/quotes/quote-list.tsx";
 
 const rootElement = document.getElementById("root");
 
@@ -10,6 +14,12 @@ if (rootElement) {
   createRoot(rootElement).render(
     <StrictMode>
       <BrowserRouter>
+        <Routes>
+          <Route index element={<LoginPage />} />
+          <Route path="cadastro" element={<CreateAccountPage />} />
+          <Route path="visao-geral" element={<DashboardPage />} />
+          <Route path="cotacoes" element={<QuoteListPage />} />
+        </Routes>
         <App />
       </BrowserRouter>
     </StrictMode>,
