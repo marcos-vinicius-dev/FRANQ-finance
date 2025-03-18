48c5daafd5348846cced25a6c186a10cf4c1c34f
Marcos Vinicius <marcos.silva@athonenergia.com.br>
2025-03-18 14:49:10 -0300

    feat: adicionar layout-protected e centralizar configuração de rotas no App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
-function App() {
+import { BrowserRouter, Route, Routes } from "react-router";
+import LoginPage from "./pages/auth/login.tsx";
+import CreateAccountPage from "./pages/auth/create-account.tsx";
+import { DashboardPage } from "./pages/quotes/dashboard.tsx";
+import { QuoteListPage } from "./pages/quotes/quote-list.tsx";
+import ProtectedLayout from "./pages/protected-layout.tsx";
 
+function App() {
   return (
     <>
-        <h1>Finance FRANQ</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<LoginPage />} />
+          <Route path="cadastro" element={<CreateAccountPage />} />
+          <Route element={<ProtectedLayout />}>
+            <Route path="visao-geral" element={<DashboardPage />} />
+            <Route path="cotacoes" element={<QuoteListPage />} />
+          </Route>
+          {/* <NotFound/> */}
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,27 +1,15 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
-import { BrowserRouter, Route, Routes } from "react-router";
+
 import "./styles/index.css";
-import LoginPage from "./pages/auth/login.tsx";
-import CreateAccountPage from "./pages/auth/create-account.tsx";
-import { DashboardPage } from "./pages/quotes/dashboard.tsx";
-import { QuoteListPage } from "./pages/quotes/quote-list.tsx";
 
 const rootElement = document.getElementById("root");
 
 if (rootElement) {
   createRoot(rootElement).render(
     <StrictMode>
-      <BrowserRouter>
-        <Routes>
-          <Route index element={<LoginPage />} />
-          <Route path="cadastro" element={<CreateAccountPage />} />
-          <Route path="visao-geral" element={<DashboardPage />} />
-          <Route path="cotacoes" element={<QuoteListPage />} />
-        </Routes>
-        <App />
-      </BrowserRouter>
+      <App />
     </StrictMode>,
   );
 } else {
