4ba70dd6d86c636be7b60a72202ca0cd2f56e961
Marcos Vinicius <marcos.silva@athonenergia.com.br>
2025-03-25 02:56:46 -0300

    perf: implementa tecnicas de code splitting

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,37 @@
 import { BrowserRouter, Route, Routes } from "react-router";
-import LoginPage from "./pages/auth/login.tsx";
-import CreateAccountPage from "./pages/auth/create-account.tsx";
-import ProtectedLayout from "./pages/protected-layout.tsx";
-import QuoteDetailPage from "./pages/finance/quote-detail";
-import DashboardPage from "./pages/finance/dashboard";
-import StockIndexListPage from "./pages/finance/stock-index-list.tsx";
-import CurrenciesListPage from "./pages/finance/currencies-list.tsx";
-import BitcoinListPage from "./pages/finance/bitcoin-list.tsx";
+import { Suspense, lazy } from "react";
+
+const LoginPage = lazy(() => import("./pages/auth/login.tsx"));
+const CreateAccountPage = lazy(() => import("./pages/auth/create-account.tsx"));
+const ProtectedLayout = lazy(() => import("./pages/protected-layout.tsx"));
+const QuoteDetailPage = lazy(() => import("./pages/finance/quote-detail"));
+const DashboardPage = lazy(() => import("./pages/finance/dashboard"));
+const StockIndexListPage = lazy(
+  () => import("./pages/finance/stock-index-list.tsx"),
+);
+const CurrenciesListPage = lazy(
+  () => import("./pages/finance/currencies-list.tsx"),
+);
+const BitcoinListPage = lazy(() => import("./pages/finance/bitcoin-list.tsx"));
 
 function App() {
   return (
     <main className="h-full min-h-screen">
       <BrowserRouter>
-        <Routes>
-          <Route index element={<LoginPage />} />
-          <Route path="cadastro" element={<CreateAccountPage />} />
-          <Route element={<ProtectedLayout />}>
-            <Route path="visao-geral" element={<DashboardPage />} />
-            <Route path=":category/:code" element={<QuoteDetailPage />} />
-            <Route path="acoes/indices" element={<StockIndexListPage />} />
-            <Route path="moedas" element={<CurrenciesListPage />} />
-            <Route path="bitcoin" element={<BitcoinListPage />} />
-          </Route>
-          {/* <NotFound/> */}
-        </Routes>
+        <Suspense fallback={<div>Carregando...</div>}>
+          <Routes>
+            <Route index element={<LoginPage />} />
+            <Route path="cadastro" element={<CreateAccountPage />} />
+            <Route element={<ProtectedLayout />}>
+              <Route path="visao-geral" element={<DashboardPage />} />
+              <Route path=":category/:code" element={<QuoteDetailPage />} />
+              <Route path="acoes/indices" element={<StockIndexListPage />} />
+              <Route path="moedas" element={<CurrenciesListPage />} />
+              <Route path="bitcoin" element={<BitcoinListPage />} />
+            </Route>
+            {/* <NotFound/> */}
+          </Routes>
+        </Suspense>
       </BrowserRouter>
     </main>
   );
