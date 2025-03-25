412b5c1ad03567acec5e6862c516466c0c50ce56
Marcos Vinicius <marcos.silva@athonenergia.com.br>
2025-03-25 02:16:46 -0300

    feat(route): atualiza rotas

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,15 @@ import { BrowserRouter, Route, Routes } from "react-router";
 import LoginPage from "./pages/auth/login.tsx";
 import CreateAccountPage from "./pages/auth/create-account.tsx";
 import ProtectedLayout from "./pages/protected-layout.tsx";
-import { QuoteDetailPage } from "./pages/finance/quote-detail";
-import { DashboardPage } from "./pages/finance/dashboard";
-import { StockIndexListPage } from "./pages/finance/stock/stock-index-list.tsx";
+import QuoteDetailPage from "./pages/finance/quote-detail";
+import DashboardPage from "./pages/finance/dashboard";
+import StockIndexListPage from "./pages/finance/stock-index-list.tsx";
 import CurrenciesListPage from "./pages/finance/currencies-list.tsx";
 import BitcoinListPage from "./pages/finance/bitcoin-list.tsx";
 
 function App() {
   return (
-    <>
+    <main className="h-full min-h-screen">
       <BrowserRouter>
         <Routes>
           <Route index element={<LoginPage />} />
@@ -18,11 +18,6 @@ function App() {
           <Route element={<ProtectedLayout />}>
             <Route path="visao-geral" element={<DashboardPage />} />
             <Route path=":category/:code" element={<QuoteDetailPage />} />
-            <Route
-              path=":category/:code/:stymbol"
-              element={<QuoteDetailPage />}
-            />
-
             <Route path="acoes/indices" element={<StockIndexListPage />} />
             <Route path="moedas" element={<CurrenciesListPage />} />
             <Route path="bitcoin" element={<BitcoinListPage />} />
@@ -30,7 +25,7 @@ function App() {
           {/* <NotFound/> */}
         </Routes>
       </BrowserRouter>
-    </>
+    </main>
   );
 }
 
