4bbfe0736620ef211709b23a18d53ba41b4408d4
Marcos Vinicius <marcos.silva@athonenergia.com.br>
2025-03-25 22:41:07 -0300

    feat(auth): adiciona formulário de login

--- src/components/auth/login-form.tsx
+++ src/components/auth/login-form.tsx
@@ -1,23 +1,71 @@
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input/inpur-text";
 import { Label } from "@/components/ui/input/label";
+import { signIn } from "@/gateway/auth";
+import { useNavigate } from "react-router";
+
+const loginSchema = z.object({
+  email: z
+    .string()
+    .min(1, "E-mail é obrigatório")
+    .email("Por favor, insira um e-mail válido"),
+  password: z
+    .string()
+    .min(1, "Senha é obrigatória")
+    .min(8, "Senha deve ter pelo menos 8 caracteres"),
+});
+
+type LoginFormData = z.infer<typeof loginSchema>;
 
 export default function LoginForm() {
+  const navigate = useNavigate();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm<LoginFormData>({
+    resolver: zodResolver(loginSchema),
+  });
+
+  const onSubmit = async ({ email, password }: LoginFormData) => {
+    try {
+      const token = await signIn(email, password);
+      token && navigate("/visao-geral");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
-    <form className="flex flex-col gap-4">
+    <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-4">
       <div>
         <Label htmlFor="email">E-mail</Label>
-        <Input id="email" type="email" placeholder="Digite seu e-mail" />
+        <Input
+          id="email"
+          type="email"
+          placeholder="Digite seu e-mail"
+          hasError={!!errors?.email}
+          errorMessage={errors?.email?.message}
+          {...register("email")}
+        />
       </div>
       <div>
         <Label htmlFor="password">Senha</Label>
         <Input
           id="password"
           type="password"
-          placeholder="Digite seu password"
+          placeholder="Digite sua senha"
+          hasError={!!errors?.password}
+          errorMessage={errors?.password?.message}
+          {...register("password")}
         />
       </div>
-      <Button className="mt-4 w-full">Entrar</Button>
+      <Button type="submit" className="mt-4 w-full" disabled={isSubmitting}>
+        {isSubmitting ? "Entrando..." : "Entrar"}
+      </Button>
     </form>
   );
 }
